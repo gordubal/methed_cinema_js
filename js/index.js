4a4879c12ed2a6de5868bef8f8199fce6c0e1ed1
Denis Bozhko <juriy_dmz@mail.ru>
2021-12-07 15:12:21 +0500

    add burger Menu

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//console.log('Hi all')
+
+import slideMenu from "./menu.js";
+
+slideMenu({
+    openBtn: '.header__burger-btn',
+    menu: '.navigation',
+    classActiveMenu: 'navigation_active',
+    closeTrigger: '.navigation__link, .navigation__close'
+})
